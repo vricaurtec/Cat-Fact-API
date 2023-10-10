0cb9349623b125a479c6033d3fef3d3a1dcaeb3e
vricaurtec <vricaurte842@gmail.com>
2023-10-10 14:47:05 -0400

    done

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const randCat_URL = "https://meowfacts.herokuapp.com/";
-
+const randCat_URL = "https://cat-fact.herokuapp.com/facts";
 let slider = document.querySelector(".slider");
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+let currentIndex = 0;
+let hatData = null;
 
 fetch(randCat_URL)
   .then((res) => res.json())
-  .then((hat) => {
-    console.log(hat);
-    slider.innerText = hat.data[0];
-  }).catch;
+  .then((data) => {
+    hatData = data;
+    console.log(hatData);
+    slider.innerText = hatData[0].text;
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+
+next.addEventListener("click", () => {
+  if (hatData && currentIndex + 1 < hatData.length) {
+    currentIndex++;
+    slider.innerText = hatData[currentIndex].text;
+    console.log(currentIndex);
+  }
+});
+
+prev.addEventListener("click", () => {
+  if (currentIndex - 1 >= 0) {
+    currentIndex--;
+    slider.innerText = hatData[currentIndex].text;
+    console.log(currentIndex);
+  }
+});
