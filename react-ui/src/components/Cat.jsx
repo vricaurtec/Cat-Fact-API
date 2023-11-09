c07a8d32c51ef4edc796a5f49bd940202ecbfae9
vricaurtec <vricaurte842@gmail.com>
2023-11-09 12:39:41 -0500

    MVP

--- react-ui/src/components/Cat.jsx
+++ react-ui/src/components/Cat.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 const Cat = () => {
   const [modalOpen, setmodalOpen] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [num, setNum] = useState(0);
 
   const fetchFacts = async () => {
     try {
@@ -36,11 +37,24 @@ const Cat = () => {
           {""}
           {facts.length > 0 && facts[0].text && (
             <div className="modalFact">
-              <p>{facts[0].text}</p>
+              <p>{facts[num].text}</p>
             </div>
           )}
         </div>
-        <button onClick={() => setmodalOpen(false)}>Close Modal</button>
+        <button
+          onClick={() => {
+            setmodalOpen(false);
+            setNum((prev) => {
+              if (prev === 4) {
+                return 0;
+              } else {
+                return prev + 1;
+              }
+            });
+          }}
+        >
+          Close Modal
+        </button>
       </Modal>
     </div>
   );
