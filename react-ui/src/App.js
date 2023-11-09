164f886f595c85b52d3a70ac47bf14ffc166db29
vricaurtec <vricaurte842@gmail.com>
2023-11-09 14:56:17 -0500

    starting css

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Cat from "./components/Cat.jsx";
+import "./App.css";
+import "./Facts.css";
 
 function App() {
   return (
     <div className="App">
       <Cat />
+      <div className="Facts">
+        <secondPage />
+      </div>
     </div>
   );
 }
